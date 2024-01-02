d3e10c52f1c9df112a1638fac37c2943d99c2cdb
naotaka nakane <n.nakane0219@gmail.com>
2024-01-02 17:50:53 +0900

    Add allocate_page to storage_manager.

--- src/storage.rs
+++ src/storage.rs
@@ -9,6 +9,7 @@ const PAGE_SIZE: u32 = 4096;
 
 pub struct StorageManager {
     data_file: File,
+    next_page_id: u32,
 }
 
 impl StorageManager {
@@ -18,7 +19,12 @@ impl StorageManager {
             .write(true)
             .create(true)
             .open(data_file_path)?;
-        Ok(Self { data_file })
+        let size = data_file.metadata()?.len();
+        let next_page_id = (size / PAGE_SIZE as u64) as u32;
+        Ok(Self {
+            data_file,
+            next_page_id,
+        })
     }
     pub fn read_page(&mut self, page_id: u32, data: &mut [u8]) -> Result<()> {
         assert!(data.len() == PAGE_SIZE as usize);
@@ -34,6 +40,14 @@ impl StorageManager {
         self.data_file.write_all(data)?;
         self.data_file.sync_all()?;
         Ok(())
+    }
+    pub fn allocate_page(&mut self) -> Result<u32> {
+        self.data_file.seek(SeekFrom::End(0))?;
+        self.data_file.write_all(&[0; PAGE_SIZE as usize])?;
+        self.data_file.sync_all()?;
+        let page_id = self.next_page_id;
+        self.next_page_id += 1;
+        Ok(page_id)
     }
 }
 
@@ -92,6 +106,24 @@ mod tests {
         let mut read_data = vec![0; PAGE_SIZE as usize];
         storage_manager.read_page(0, &mut read_data)?;
         assert_eq!(read_data, vec![1; PAGE_SIZE as usize]);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_allocate_page() -> Result<()> {
+        let dir = tempdir()?;
+        let data_file_path = dir.path().join("data");
+        let mut storage_manager = StorageManager::new(data_file_path.to_str().unwrap())?;
+
+        let page_id1 = storage_manager.allocate_page()?;
+        let page_id2 = storage_manager.allocate_page()?;
+        assert_eq!(page_id1, 0);
+        assert_eq!(page_id2, 1);
+
+        let mut storage_manager = StorageManager::new(data_file_path.to_str().unwrap())?;
+        let page_id3 = storage_manager.allocate_page()?;
+        assert_eq!(page_id3, 2);
 
         Ok(())
     }
