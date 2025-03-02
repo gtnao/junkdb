da6c35dc7b6b2ae8bc4e315feceb4a26a4d5da69
naotaka nakane <n.nakane0219@gmail.com>
2025-03-02 03:33:03 +0000

    Clean.

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,3 @@
-use std::process;
-
-use anyhow::Result;
-use junkdb::{client::client_start, server::server_start};
-
-fn main() -> Result<()> {
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() < 2 {
-        println!("Usage: cargo run client|server");
-        process::exit(1);
-    }
-    match &*args[1] {
-        "client" => client_start()?,
-        "server" => {
-            let init = args.iter().any(|arg| arg == "--init");
-            let recover = args.iter().any(|arg| arg == "--recover");
-            server_start(init, recover)?;
-        }
-        _ => {
-            println!("Usage: cargo run client|server");
-            process::exit(1);
-        }
-    }
-    Ok(())
+fn main() {
+    println!("Hello, world!");
 }
