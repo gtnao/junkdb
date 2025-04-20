7fca7c86a9600e00b468b4e3754d4dcab4809c38
naotaka nakane <n.nakane0219@gmail.com>
2025-04-20 02:34:59 +0000

    Add PageManager.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    fs::{File, OpenOptions},
+    path::Path,
+};
+
+use anyhow::Result;
+
+struct PageManager {
+    file: File,
+}
+
+impl PageManager {
+    fn load<T: AsRef<Path>>(file_path: T) -> Result<Self> {
+        Ok(Self {
+            file: OpenOptions::new()
+                .read(true)
+                .write(true)
+                .open(file_path.as_ref())?,
+        })
+    }
+    fn bootstrap<T: AsRef<Path>>(file_path: T) -> Result<Self> {
+        Ok(Self {
+            file: OpenOptions::new()
+                .read(true)
+                .write(true)
+                .create(true)
+                .truncate(true)
+                .open(file_path.as_ref())?,
+        })
+    }
+}
+
+fn main() -> Result<()> {
+    let page_manager = PageManager::bootstrap("example.db")?;
+    let page_manager2 = PageManager::load("example.db")?;
+    Ok(())
 }
